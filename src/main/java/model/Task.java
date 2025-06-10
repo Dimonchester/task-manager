e29662f66d0b965d0ea1bbe7dde3038487e52c43
Dimonchester <dimonchester.tkachov@gmail.com>
2025-06-10 21:24:20 +0300

    first commit

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    // private Priority priority;
+    private LocalDate deadline;
+    private List<String> tags;
+
+    // Геттеры/сеттеры + конструктор
+}
