c7bba283c03eaa53baaca9831ec411246ded3b04
Dimonchester <dimonchester.tkachov@gmail.com>
2025-06-19 16:21:45 +0300

    create shteynzkiii

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
-package org.example;
+import model.Task;
+import algorithm.EDFScheduler;
+import java.util.Date;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EDFScheduler scheduler = new EDFScheduler();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Добавляем задачи с разными дедлайнами
+        Task task1 = new Task("T1", new Date().getTime() + 5000, 1);
+        Task task2 = new Task("T2", new Date().getTime() + 3000, 2);
+        Task task3 = new Task("T3", new Date().getTime() + 7000, 3);
+
+        scheduler.addTask(task1);
+        scheduler.addTask(task2);
+        scheduler.addTask(task3);
+
+        // Обрабатываем задачи в порядке EDF
+        while (scheduler.hasTasks()) {
+            Task nextTask = scheduler.getNextTask();
+            System.out.println("Processing: " + nextTask.getId());
         }
     }
 }
